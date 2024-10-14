2edcc7ca325bd9359f3e057b2c76f18328722c75
Major <107882188+qubitflip@users.noreply.github.com>
2024-10-13 22:52:09 -0400

    Food selection next

---
+++ client/src/pages/onboarding/SelectFood.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SelectFood = () => {
+	return (
+		<div>
+			TODO: Food they already have selection screen.
+		</div>
+	);
+}
+
+export default SelectFood;
---
+++ client/src/pages/onboarding/food_list.js
@@ -0,0 +1,1 @@
+// Common food items that people have.
